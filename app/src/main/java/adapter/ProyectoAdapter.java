831e79a7c3dc291134a9583f01cbac41024c71a5
florenciaanahi <43067809+florenciaanahi@users.noreply.github.com>
2018-10-09 17:43:41 -0300

    proyecto adapter

---
+++ app/src/main/java/adapter/ProyectoAdapter.java
@@ -0,0 +1,63 @@
+package adapter;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.provider.BaseColumns;
+import android.provider.FontsContract;
+
+
+public class ProyectoAdapter {
+    private static final String NAME ="PROYECTO";
+    private SQLiteDatabase sqlDB;
+
+    public ProyectoAdapter (SQLiteDatabase sqlDB){
+        this.sqlDB =sqlDB;
+    }
+
+    private class Columns implements BaseColumns{
+        public final static String ID ="proyecto_id";
+        public final static String DESCRIPCION="descripcion";
+        public final static String COSTO="costo";
+        public final static String CLIENTE_ID="cliente_id";
+    }
+    private final static String [] COLUMNS ={
+            Columns.ID,Columns.DESCRIPCION,Columns.COSTO,Columns.CLIENTE_ID
+    };
+    public final static String CR_TABLE = "create table if not exist"
+    + NAME + "(" + Columns.ID
+    + "integer primary key autoincrement,"
+    + Columns.DESCRIPCION+ "TEXTO NULL,"
+    + Columns.COSTO + "real,"
+    +Columns.CLIENTE_ID + "INTEGER FOREING KEY (cliente id) REFERENCES cliente (cliente_id))";
+
+    public boolean Insert (String descripcion, double costo, int cliente_id){
+        ContentValues values = new ContentValues();
+        values.put(Columns.DESCRIPCION, descripcion);
+        values.put(Columns.COSTO, costo);
+        values.put(Columns.CLIENTE_ID, cliente_id);
+
+    return sqlDB.insert(NAME, null, values)> 0;
+    }
+    public Cursor getDescripciones(){
+        String[] columns={Columns.DESCRIPCION};
+        return sqlDB.query(NAME,columns,null,null,null,null,null);
+
+        }
+
+    public static String getNAME() {
+        return NAME;
+    }
+
+    public static String[] getCOLUMNS() {
+        return COLUMNS;
+    }
+
+    public  boolean isEmpty(){
+        return sqlDB.query(NAME, COLUMNS,null,null,null,null,null). getCount()==0;
+
+    }
+    public Cursor getDatos(){
+        return sqlDB.query(NAME, COLUMNS,null,null,null,null,null);
+    }
+}
+
