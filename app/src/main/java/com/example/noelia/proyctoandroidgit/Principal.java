bb07e9aea04e74d7892720aee96d42d683a02773
florenciaanahi <43067809+florenciaanahi@users.noreply.github.com>
2018-10-16 15:31:09 -0300

    layout_activity

---
+++ app/src/main/java/com/example/noelia/proyctoandroidgit/Principal.java
@@ -0,0 +1,52 @@
+package com.example.noelia.proyctoandroidgit;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Principal extends AppCompatActivity {
+
+    private EditText nombre;
+    private EditText apellido;
+    private EditText telefono;
+    private Button guardar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_activity);
+        nombre= (EditText)findViewById(R.id.etnombre);
+        apellido=(EditText)findViewById(R.id.etapellido);
+        telefono=(EditText)findViewById(R.id.ettelefono);
+        guardar=(Button)findViewById(R.id.btnguardar);
+        guardar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                ((BaseApplication)getApplication()).insertarCliente(nombre.getText().toString(),
+                        apellido.getText().toString(),
+                        telefono.getText().toString());
+                Intent in=new Intent (Principal.this,ListActivity.class);
+                startActivity(in);
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(MenuItem item) {
+        int id= item.getItemId();
+        if(id == R.id.action_Settings){
+            return true;
+        }
+return super.onOptionsItemSelected(item);
+    }
+}
